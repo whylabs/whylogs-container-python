0a6c732c6f2348fe05c49378ca1a2c74e1a7c311
Anthony Naddeo <anthony@whylabs.ai>
2023-02-20 16:05:11 -0800

    Refactor dockerfile, add fail mode when missing config
    Updated docker files to ignore most of the bulky local content when
    copying build context and moved the src copy to a later layer so that
    source updates don't trigger apt-get and poetry layers anymore.
    
    Also add a new env var to fail the container startup if the config is
    missing as a safeguard to image building mistakes.

--- Dockerfile
+++ Dockerfile
@@ -1,27 +1,31 @@
+##
 ## Install/build dependencies from apt and pip
-
+##
 FROM ubuntu:22.04 as core_dependencies
 RUN apt-get update && apt-get install -y python3.10 ca-certificates
-# Copy src
-COPY src /opt/whylogs-container/src
-COPY poetry.lock /opt/whylogs-container/
-COPY pyproject.toml /opt/whylogs-container/
-COPY README.md /opt/whylogs-container/
-COPY whylabs_toolkit-0.0.1-py3-none-any.whl /opt/whylogs-container/
 
+##
 ## Install/build pip dependencies
+##
 FROM core_dependencies as python_dependencies
 RUN apt-get install -y curl build-essential python3-dev
 # Install poetry
 RUN curl -sSL https://install.python-poetry.org | python3.10 -
+# Copy poetry files over for python dependencies
+COPY poetry.lock /opt/whylogs-container/
+COPY pyproject.toml /opt/whylogs-container/
+COPY whylabs_toolkit-0.0.1-py3-none-any.whl /opt/whylogs-container/
 WORKDIR /opt/whylogs-container
 RUN /root/.local/bin/poetry config virtualenvs.in-project true
-RUN /root/.local/bin/poetry install --without=dev
+RUN /root/.local/bin/poetry install --no-root --without=dev
 # Pandas deploys a ton of tests to pypi
 RUN rm -rf .venv/lib/python3.10/site-packages/pandas/tests
 
-## Drop extra things needed only to install/build and reinstall runtime requirements
+##
+## Copy required files from previous steps and copy src over
+##
 FROM core_dependencies
 WORKDIR /opt/whylogs-container
 COPY --from=python_dependencies /opt/whylogs-container ./
+COPY src /opt/whylogs-container/src
 ENTRYPOINT [ "/bin/bash", "-c", "source .venv/bin/activate; cd src; python3.10 -m ai.whylabs.container.startup" ]
--- src/ai/whylabs/container/config.py
+++ src/ai/whylabs/container/config.py
@@ -9,7 +9,61 @@ from whylabs_toolkit.container.config_types import DatasetCadence, DatasetOption
 _logger = logging.getLogger("config")
 
 
+class EnvVarNames(Enum):
+    WHYLABS_API_KEY = "WHYLABS_API_KEY"
+    WHYLABS_ORG_ID = "WHYLABS_ORG_ID"
+    DEFAULT_WHYLABS_DATASET_CADENCE = "DEFAULT_WHYLABS_DATASET_CADENCE"
+    DEFAULT_WHYLABS_UPLOAD_CADENCE = "DEFAULT_WHYLABS_UPLOAD_CADENCE"
+    DEFAULT_WHYLABS_UPLOAD_INTERVAL = "DEFAULT_WHYLABS_UPLOAD_INTERVAL"
+
+    FAIL_STARTUP_WITHOUT_CONFIG = "FAIL_STARTUP_WITHOUT_CONFIG"
+
+
+class ContainerConfig:
+    whylabs_api_key: str
+    whylabs_org_id: str
+
+    default_dataset_cadence: DatasetCadence
+    default_whylabs_upload_cadence: DatasetUploadCadenceGranularity
+    default_whylabs_upload_interval: int
+
+    fail_startup_without_config: bool
+
+    def __init__(self) -> None:
+        self.whylabs_api_key = self._require_env(EnvVarNames.WHYLABS_API_KEY)
+        self.whylabs_org_id = self._require_env(EnvVarNames.WHYLABS_ORG_ID)
+
+        self.fail_startup_without_config = self._read_env(EnvVarNames.FAIL_STARTUP_WITHOUT_CONFIG) == 'True'
+
+        default_dataset_cadence = self._read_env(EnvVarNames.DEFAULT_WHYLABS_DATASET_CADENCE)
+        self.default_dataset_cadence = default_dataset_cadence and DatasetCadence[default_dataset_cadence] or DatasetCadence.DAILY
+
+        default_whylabs_upload_cadence = self._read_env(EnvVarNames.DEFAULT_WHYLABS_UPLOAD_CADENCE)
+        self.default_whylabs_upload_cadence = (
+            default_whylabs_upload_cadence
+            and DatasetUploadCadenceGranularity[default_whylabs_upload_cadence]
+            or DatasetUploadCadenceGranularity.HOUR
+        )
+
+        default_whylabs_upload_interval = self._read_env(EnvVarNames.DEFAULT_WHYLABS_UPLOAD_INTERVAL)
+        self.default_whylabs_upload_interval = default_whylabs_upload_interval and int(default_whylabs_upload_interval) or 1
+
+    def _require_env(self, var: EnvVarNames) -> str:
+        try:
+            return os.environ[var.name]
+        except KeyError as e:
+            _logger.error(f"Missing value for env var {var.name}")
+            raise e
+
+    def _read_env(self, var: EnvVarNames) -> Optional[str]:
+        try:
+            return os.environ[var.name]
+        except KeyError:
+            return None
+
+
 def _load_custom_options() -> Optional[Dict[str, DatasetOptions]]:
+    config = ContainerConfig()
     try:
         # This may or may not exist depending on if the use supplies custom configuration and
         # builds it into a downstream docker image
@@ -18,8 +72,13 @@ def _load_custom_options() -> Optional[Dict[str, DatasetOptions]]:
         s: Dict[str, DatasetOptions] = schemas  # TODO why is this cast required?
         _logger.info(f"Found custom whylogs configuration {s}")
         return s
-    except (ImportError, ModuleNotFoundError):
+    except (ImportError, ModuleNotFoundError) as e:
         _logger.warning("No custom whylogs configuration found.")
+        if config.fail_startup_without_config:
+            _logger.exception(
+                "Failing startup because no custom whylogs configuration was found and FAIL_STARTUP_WITHOUT_CONFIG env var is True", e
+            )
+            os._exit(1)
         return None
     except Exception as e:
         _logger.exception("Couldn't import custom configuration", e)
@@ -36,54 +95,3 @@ def get_dataset_options(dataset_id: str) -> Optional[DatasetOptions]:
     except KeyError:
         _logger.info(f"No custom configuration for dataset {dataset_id}")
         return None
-
-
-class EnvVarNames(Enum):
-    WHYLABS_API_KEY = "WHYLABS_API_KEY"
-    WHYLABS_ORG_ID = "WHYLABS_ORG_ID"
-    DEFAULT_WHYLABS_DATASET_CADENCE = "DEFAULT_WHYLABS_DATASET_CADENCE"
-    DEFAULT_WHYLABS_UPLOAD_CADENCE = "DEFAULT_WHYLABS_UPLOAD_CADENCE"
-    DEFAULT_WHYLABS_UPLOAD_INTERVAL = "DEFAULT_WHYLABS_UPLOAD_INTERVAL"
-
-
-class ContainerConfig:
-    whylabs_api_key: str
-    whylabs_org_id: str
-
-    default_dataset_cadence: DatasetCadence
-    default_whylabs_upload_cadence: DatasetUploadCadenceGranularity
-    default_whylabs_upload_interval: int
-
-    def __init__(self) -> None:
-        self.whylabs_api_key = self._require_env(EnvVarNames.WHYLABS_API_KEY)
-        self.whylabs_org_id = self._require_env(EnvVarNames.WHYLABS_ORG_ID)
-
-        default_dataset_cadence = self._read_env(EnvVarNames.DEFAULT_WHYLABS_DATASET_CADENCE)
-        self.default_dataset_cadence = (
-            default_dataset_cadence and DatasetCadence[default_dataset_cadence] or DatasetCadence.DAILY
-        )
-
-        default_whylabs_upload_cadence = self._read_env(EnvVarNames.DEFAULT_WHYLABS_UPLOAD_CADENCE)
-        self.default_whylabs_upload_cadence = (
-            default_whylabs_upload_cadence
-            and DatasetUploadCadenceGranularity[default_whylabs_upload_cadence]
-            or DatasetUploadCadenceGranularity.HOUR
-        )
-
-        default_whylabs_upload_interval = self._read_env(EnvVarNames.DEFAULT_WHYLABS_UPLOAD_INTERVAL)
-        self.default_whylabs_upload_interval = (
-            default_whylabs_upload_interval and int(default_whylabs_upload_interval) or 1
-        )
-
-    def _require_env(self, var: EnvVarNames) -> str:
-        try:
-            return os.environ[var.name]
-        except KeyError as e:
-            _logger.error(f"Missing value for env var {var.name}")
-            raise e
-
-    def _read_env(self, var: EnvVarNames) -> Optional[str]:
-        try:
-            return os.environ[var.name]
-        except KeyError:
-            return None
